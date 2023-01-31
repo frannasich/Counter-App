34e6dfb35aa0f439429f6a9d4d001e5b4c980e9f
Francisco <francisco.nasich@mercadolibre.com>
2023-01-30 21:07:30 -0300

    Refactorized code

--- lib/screens/counter_screen.dart
+++ lib/screens/counter_screen.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 class CounterScreen extends StatefulWidget {
@@ -12,12 +11,18 @@ class _CounterScreenState extends State<CounterScreen> {
 
   int counter = 0;
 
+  void increase() => setState(() => counter++);
+  void decrease() => setState(() => counter--);
+  void reset() => setState(() => counter = 0);
+
   @override
   Widget build(Object context) {
-    const fontSize30 = TextStyle(fontSize: 30);
+    const fontSize30 = TextStyle(fontSize: 30, color: Colors.white);
 
     return Scaffold(
+      backgroundColor: Colors.grey[850],
       appBar: AppBar(
+        backgroundColor: Colors.red[400],
         title: const Text('Counter Screen'),
         elevation: 0,
       ),
@@ -32,42 +37,55 @@ class _CounterScreenState extends State<CounterScreen> {
         ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.miniCenterDocked,
-      floatingActionButton: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          FloatingActionButton(
-            child: const Icon(Icons.exposure_minus_1_rounded),
-            onPressed: () {
-              setState(() {
-                counter --;
-              });
-            }
-            ),
-
-            // const SizedBox(width: 20,),
-
-
-            FloatingActionButton(
-            child: const Icon(Icons.restart_alt),
-            onPressed: () {
-              setState(() {
-                counter = 0;
-              });
-            }
-            ),
-
-            // const SizedBox(width: 20,),
-
-            FloatingActionButton(
-            child: const Icon(Icons.exposure_plus_1_rounded),
-            onPressed: () {
-              setState(() {
-                counter ++;
-              });
-            }
-            )
-        ],
+      floatingActionButton: CustomFloatingActions(
+        increaseFn: increase,
+        decreaseFn: decrease,
+        resetFn: reset,
       ), 
+    );
+  }
+}
+
+class CustomFloatingActions extends StatelessWidget {
+
+  final Function increaseFn;
+  final Function decreaseFn;
+  final Function resetFn;
+
+  const CustomFloatingActions({
+    super.key, 
+    required this.increaseFn, 
+    required this.decreaseFn, 
+    required this.resetFn,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+
+        // Plus Button
+        FloatingActionButton(
+          backgroundColor: Colors.red[400],
+          child: const Icon(Icons.exposure_minus_1_rounded),
+          onPressed: () => decreaseFn()
+        ),
+
+        // Reset Button
+        FloatingActionButton(
+          backgroundColor: Colors.red[400],
+          child: const Icon(Icons.restart_alt),
+          onPressed: () => resetFn()
+        ),
+
+        // Minus Button
+        FloatingActionButton(            
+          backgroundColor: Colors.red[400],
+          child: const Icon(Icons.exposure_plus_1_rounded),
+          onPressed: () => increaseFn()
+        )
+      ],
     );
   }
 }
